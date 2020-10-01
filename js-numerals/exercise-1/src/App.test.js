9b5ef63538f5b96a77e78bd6a9a7d8fe11b85e29
minh9 <minh930906@gmail.com>
2020-10-01 16:14:58 +0200

    spelling from 1 to 99 works

--- js-numerals/exercise-1/src/App.test.js
+++ js-numerals/exercise-1/src/App.test.js
@@ -6,7 +6,7 @@ it("spell one digit number", () => {
   expect(spellNum(8)).toBe("eight");
 });
 
-it("spell 2 digit numbers", () => {
+it("spell 2 digit numbers between 10 and 20", () => {
   expect(spellNum(16)).toBe("sixteen");
   expect(spellNum(17)).toBe("seventeen");
   expect(spellNum(19)).toBe("nineteen");
@@ -16,4 +16,10 @@ it("spell 2 digit numbers ending with -ty", () => {
   expect(spellNum(70)).toBe("seventy");
   expect(spellNum(80)).toBe("eighty");
   expect(spellNum(90)).toBe("ninety");
+});
+
+it("spell 2 digit numbers between 20 and 99", () => {
+  expect(spellNum(65)).toBe("sixty-five");
+  expect(spellNum(73)).toBe("seventy-three");
+  expect(spellNum(94)).toBe("ninety-four");
 });
