f0516d66a86e567bcef438aaf9fb7a9d3ba3f54e
minh9 <minh930906@gmail.com>
2020-10-01 16:06:59 +0200

    spelling two digit number works

--- js-numerals/exercise-1/src/App.test.js
+++ js-numerals/exercise-1/src/App.test.js
@@ -5,3 +5,9 @@ it("spell one digit number", () => {
   expect(spellNum(5)).toBe("five");
   expect(spellNum(8)).toBe("eight");
 });
+
+it("spell 2 digit numbers", () => {
+  expect(spellNum(16)).toBe("sixteen");
+  expect(spellNum(17)).toBe("seventeen");
+  expect(spellNum(19)).toBe("nineteen");
+});
--- js-numerals/exercise-1/src/utilCalculation.js
+++ js-numerals/exercise-1/src/utilCalculation.js
@@ -24,6 +24,28 @@ var numbers = {
   1000000000: "billion",
 };
 
+const spellNumBetweenTenAndTwenty = (num, remainder) => {
+  return (numbers[num] = numbers[remainder].concat("teen"));
+};
+
 export const spellNum = (num) => {
+  for (var iterNum = 0; iterNum <= num; iterNum++) {
+    if (iterNum in numbers) {
+      continue;
+    }
+
+    var numberLength = iterNum.toString().length - 1;
+
+    var firstDivideNum = "1";
+
+    for (var x = 0; x < numberLength; x++) {
+      firstDivideNum = firstDivideNum.concat("0");
+    }
+    var remainder = iterNum % firstDivideNum;
+
+    if (10 < iterNum && iterNum < 20 && !(iterNum in numbers)) {
+      numbers[iterNum] = spellNumBetweenTenAndTwenty(iterNum, remainder);
+    }
+  }
   return numbers[num];
 };
